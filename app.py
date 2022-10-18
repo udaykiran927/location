53bc614a73c8f445fb8baadb94bb61bf85626c15
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2022-10-18 20:29:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask,render_template,redirect
-from urllib.request import urlopen
+from flask import Flask,render_template,redirect,request
 import json
 
 
 app=Flask(__name__)
 @app.route("/")
 def home():
-    
+
     return render_template("location.html")
+@app.route("/predict",methods=["POST","GET"])
+def predict():
+    val=[i for i in request.form.values()]
+    print(val)
+    if float(val[0])<=14 and float(val[1]):
+        atten="Present"
+    else:
+        atten="Absent"
+    return render_template("location.html",val=val,attend="Your attendence marked as: "+atten)
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
+
