853ff46cb6b41528d1caf4287b7ca3bf647cfaa3
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2022-10-18 22:19:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
 def predict():
     val=[i for i in request.form.values()]
     print(val)
-    if((float(val[0])<=14 and float(val[0])>=13) and (float(val[1])<=79 and float(val[1])>=78)):
+    if((float(val[0])<=13.62960313 and float(val[1])>=13.62940313)):
         atten="Present"
     else:
         atten="Absent"
