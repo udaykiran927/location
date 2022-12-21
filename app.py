8c3a0ce3d6b61c1cdc051657901c0a50aba0307d
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2022-12-21 14:24:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask,render_template,redirect,request
 import json
+import pandas as pd
 
 
 app=Flask(__name__)
+lat=[]
+lon=[]
+place=[]
 @app.route("/")
 def home():
 
@@ -11,8 +15,12 @@ def home():
 def predict():
     val=[i for i in request.form.values()]
     print(val)
+    lat.append(val[0])
+    lon.append(val[1])
+    place.append(val[2])
     Latval=val[0]
     Lonval=val[1]
+    
     if((float(val[0])<=13.62960313 and float(val[1])>=13.62940313)):
         atten="Present"
     else:
@@ -20,7 +28,14 @@ def predict():
     return render_template("location.html",Latval=Latval,Lonval=Lonval,attend="Your attendence marked as: "+atten)
 
 
+@app.route("/download",methods=["POST","GET"])
 
+def download():
+    d={"lattitude":lat,"longitude":lon,"place":place}
+    print(d)
+    df=pd.DataFrame(d)
+    df.to_csv(r'C:\\Users\\udayk\\Desktop\\user2.csv')
+    return render_template("location.html",msg="Coordinates Downloaded")
 if __name__=='__main__':
     app.run(debug=True)
 
